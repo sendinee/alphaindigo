e027d5196d7d1b93371be05e83e06ffa78b5398a
sendinee <48397577+sendinee@users.noreply.github.com>
2020-02-02 18:00:45 +0100

    Update Generale.java
    hebergemnt-->logement

--- Corsair0/src/vue/Generale.java
+++ Corsair0/src/vue/Generale.java
@@ -1,79 +1,100 @@
 package vue;
 
-import java.awt.Component;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
-import controleur.User;
+import controleur.Corsair;
 
-public class Generale extends JFrame 
+public class PanelMenu extends JPanel implements ActionListener
 {
-	private static PanelMenu unPanelMenu = new PanelMenu();
-	private static PanelClient unPanelClient = new PanelClient(); 
-	private static PanelHebergement unPanelHebergement= new PanelHebergement(); 
-	private static PanelLocation unPanelLocation = new PanelLocation(); 
-	private static PanelVol unPanelVol = new PanelVol();
+	private JButton btClient = new JButton("Client");
+	private JButton btLogement= new JButton("Logement");
+	private JButton btLocation = new JButton("Location");
+	private JButton btVol = new JButton("Vol");
+	private JButton btQuitter = new JButton("Quitter");
+	private JButton btProfil = new JButton("Mon Profil");
 	
-	private static PanelProfil unPanelProfil; 
-	
-	public Generale (User unUser)
+	public PanelMenu ()
 	{
-		unPanelProfil = new PanelProfil(unUser);
+		this.setBackground(Color.PINK);
+		this.setBounds(20, 20, 120, 340);
+		this.setLayout(new GridLayout(6, 1));
 		
-		this.setTitle("Gestion de Stock");
-		this.setBounds(200, 200, 700, 400);
-		this.setResizable(false);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLayout(null);
+		this.add (this.btProfil);
+		this.add (this.btClient);
+		this.add (this.btLogement);
+		this.add (this.btLocation);
+		this.add (this.btVol);
+		this.add (this.btQuitter);
 		
-		this.add(unPanelMenu);
-		this.add(unPanelClient);
-		this.add(unPanelHebergement);
-		this.add(unPanelLocation);
-		this.add(unPanelVol);
-		this.add(unPanelProfil);
+		this.btProfil.addActionListener(this);
+		this.btClient.addActionListener(this);
+		this.btLogement.addActionListener(this);
+		this.btLocation.addActionListener(this);
+		this.btVol.addActionListener(this);
+		this.btQuitter.addActionListener(this);
 		
 		this.setVisible(true);
 	}
-	private void add(PanelProfil unPanelProfil) {
-		// TODO Auto-generated method stub
-		
-	}
-	public static void setVisibleProfil(boolean action)
-	{
-		Generale.unPanelProfil.setVisible(action);
-	}
-	public static void setVisibleClient(boolean action)
-	{
-		Generale.unPanelClient.setVisible(action);
-	}
-	public static void setVisibleHebergement(boolean action)
-	{
-		Generale.unPanelHebergement.setVisible(action);
-	}
-	public static void setVisibleLocation(boolean action)
-	{
-		if(action == true)
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() ==  this.btProfil)
 		{
-			//actualiser le CBX
-			unPanelLocation.actualise(); 
-			
-			
+			Generale.setVisibleProfil(true);
+			Generale.setVisibleClient(false);
+			Generale.setVisibleLogement(false);
+			Generale.setVisibleLocation(false);
+			Generale.setVisibleVol(false);
 		}
-		Generale.unPanelLocation.setVisible(action);
-	}
-	public static void setVisibleVol(boolean action)
-	{
-		Generale.unPanelVol.setVisible(action);
-	}
-	public static PanelClient getUnPanelClient() {
-		return unPanelClient;
-	}
-	public static void setUnPanelClient(PanelClient unPanelClient) {
-		Generale.unPanelClient = unPanelClient;
-	}
-	public static PanelClient getUnLister() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		
+		else if (e.getSource() ==  this.btClient)
+		{
+			Generale.setVisibleProfil(false);
+			Generale.setVisibleClient(true);
+			Generale.setVisibleLogement(false);
+			Generale.setVisibleLocation(false);
+			Generale.setVisibleVol(false);
+		}
+		
+		else if (e.getSource() == this.btLogement)
+		{
+			Generale.setVisibleClient(false);
+			Generale.setVisibleLogement(true);
+			Generale.setVisibleLocation(false);
+			Generale.setVisibleVol(false);
+		}
+		
+		else if (e.getSource() == this.btVol)
+		{
+			Generale.setVisibleProfil(false);
+			Generale.setVisibleClient(false);
+			Generale.setVisibleLogement(false);
+			Generale.setVisibleLocation(false);
+			Generale.setVisibleVol(true);
+		}else if (e.getSource() == this.btLocation)
+		{
+			Generale.setVisibleProfil(false);
+			Generale.setVisibleClient(false);
+			Generale.setVisibleLogement(false);
+			Generale.setVisibleLocation(true);
+			Generale.setVisibleVol(false);
+		}
+		
+		else if (e.getSource() == this.btQuitter)
+		{
+			int retour = JOptionPane.showConfirmDialog(this, "Voulez-vous quitter l'application ?", "Quitter Application", JOptionPane.YES_NO_OPTION);
+			if (retour == 0)
+			{
+				Corsair.fin(); //fin de générale
+				Corsair.rendreVisibleConnexion(true);
+			}
+		}
+	}	
 }
