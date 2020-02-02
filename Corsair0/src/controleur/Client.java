108c99301d76406b7d68531b53b6cda8ad91034b
sendinee <48397577+sendinee@users.noreply.github.com>
2020-02-02 20:40:53 +0100

    Update Client.java

--- Corsair0/src/controleur/Client.java
+++ Corsair0/src/controleur/Client.java
@@ -1,13 +1,13 @@
 package controleur;
 
 public class Client {
-		private String idclient, nom, prenom, email; 
-		private int numero_passeport, tel, mdp, age; 
+		private String idclient, nom, prenom, adresse, email, numero_passeport, tel, mdp, age; 
 		
-		public Client(String idclient,String nom, String prenom, String email, int numero_passeport, int tel, int mdp, int age) {
+		public Client(String idclient,String nom, String prenom, String adresse, String email, String numero_passeport, String tel, String mdp, String age) {
 			this.idclient = idclient;
 			this.nom = nom;
 			this.prenom = prenom;
+			this.adresse = adresse;
 			this.email = email;
 			this.numero_passeport = numero_passeport;
 			this.tel = tel;
@@ -32,36 +32,40 @@ public class Client {
 		public void setPrenom(String prenom) {
 			this.prenom = prenom;
 		}
+		public String getAdresse() {
+			return adresse;
+		}
+		public void setAdresse(String adresse) {
+			this.adresse = adresse;
+		}
 		public String getEmail() {
 			return email;
 		}
 		public void setEmail(String email) {
 			this.email = email;
 		}
-		public int getNumero_passeport() {
+		public String getNumero_passeport() {
 			return numero_passeport;
 		}
-		public void setNumero_passeport(int numero_passeport) {
+		public void setNumero_passeport(String numero_passeport) {
 			this.numero_passeport = numero_passeport;
 		}
-		public int getTel() {
+		public String getTel() {
 			return tel;
 		}
-		public void setTel(int tel) {
+		public void setTel(String tel) {
 			this.tel = tel;
 		}
-		public int getMdp() {
+		public String getMdp() {
 			return mdp;
 		}
-		public void setMdp(int mdp) {
+		public void setMdp(String mdp) {
 			this.mdp = mdp;
 		}
-		public int getAge() {
+		public String getAge() {
 			return age;
 		}
-		public void setAge(int age) {
+		public void setAge(String age) {
 			this.age = age;
 		}
-		
-
 }
